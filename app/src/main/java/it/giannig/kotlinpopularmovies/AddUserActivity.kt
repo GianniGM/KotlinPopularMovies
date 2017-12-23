39f17d1ccd439e88bf7cf7be1896fd98aa92eff0
Gianni G <giangraziano@gmail.com>
2017-12-23 17:20:38 +0100

    added addUser widgets

--- app/src/main/java/it/giannig/kotlinpopularmovies/AddUserActivity.kt
+++ app/src/main/java/it/giannig/kotlinpopularmovies/AddUserActivity.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_add_user.*
 import java.io.Serializable
-import kotlinx.android.synthetic.main.activity_user_detail.*
+
+val PICK_IMAGE = 1;
 
 class AddUserActivity : AppCompatActivity() {
 
@@ -14,11 +16,23 @@ class AddUserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_user)
 
+        person_image.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+
         submit_button.setOnClickListener {
             val intent = Intent()
             intent.putExtra(EXTRA_ADD_USER, User("ciccio") as Serializable)
             setResult(Activity.RESULT_OK, intent)
             finish()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if( requestCode == PICK_IMAGE)
+            Log.d("PICK_IMAGE", "image picked")
     }
 }
